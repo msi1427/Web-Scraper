9d8c2e59c388d942dbf6c8838f4e5085612ff167
msi1427 <sabikirbaz@iu-dhaka.edu>
2020-03-21 09:54:27 +0600

    Can Crawl, store data in files and databases

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -10,5 +10,7 @@ import scrapy
 
 class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    author = scrapy.Field()
+    tag = scrapy.Field()
+
